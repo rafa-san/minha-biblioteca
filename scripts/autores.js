dd7bae834bfd6007eb4afee00a31d7d454633cda
Rafael <88090582+rafa-san@users.noreply.github.com>
2024-02-21 11:22:08 -0300

    Criei o script dos autores lidos

---
+++ scripts/autores.js
@@ -0,0 +1,27 @@
+const autores = [
+  "Alix Barzelay",
+  "Anthony Browne",
+  "CárcamO",
+  "Carolina Moreyra",
+  "Chloé Legeay",
+  "David Merveille",
+  "Davide Cali",
+  "François Place",
+  "Frédéric Gros",
+  "Giovanna Zoboli",
+  "James Joyce",
+  "Jennifer Uman",
+  "Juta Bauer",
+  "Liev Tolstói",
+  "Maurice Sendak",
+  "Pep Molist",
+  "Raja Mohanty",
+  "Shel Silverstein",
+  "Sin-léqi-unnínni",
+  "Sirish Rao",
+  "Suzy Lee",
+  "Valerio Vidali"
+];
+
+// Exibir a quantidade de autores
+document.getElementById('total-autores').innerHTML = autores.length;
