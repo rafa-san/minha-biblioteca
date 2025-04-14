26963d6e006feb32215ca01d61ee1ab84068c26a
Rafael Santana <88090582+rafa-san@users.noreply.github.com>
2025-04-14 10:29:46 -0300

    style: melhorias na indentação

--- scripts/frases-e-trechos-1.js
+++ scripts/frases-e-trechos-1.js
@@ -1,62 +1,55 @@
 const selecao = [
-  {frase: "Um livro pode ajudar você a entender melhor a vida.", autor: "Chloé Legeay", livro: "Para que serve um livro?"},
+  { frase: "Um livro pode ajudar você a entender melhor a vida.", autor: "Chloé Legeay", livro: "Para que serve um livro?" },
 
-  {frase: "O mundo é um círculo infinito, em que tudo tem um tempo e um lugar. Mesmo algo de extrema beleza chega necessariamente a um fim e renasce como algo diferente. Existe uma ordem. Se você quiser mudá-la, será preciso agir de acordo com o que seu coração dita. Mas, no fim, é você que pertence à ordem. Ela não pertence a você.", autor: "Raja Mohanty, Sirish Rao", livro: "O círculo do destino"},
+  { frase: "O mundo é um círculo infinito, em que tudo tem um tempo e um lugar. Mesmo algo de extrema beleza chega necessariamente a um fim e renasce como algo diferente. Existe uma ordem. Se você quiser mudá-la, será preciso agir de acordo com o que seu coração dita. Mas, no fim, é você que pertence à ordem. Ela não pertence a você.", autor: "Raja Mohanty, Sirish Rao", livro: "O círculo do destino" },
 
-  {frase: "Aprenda a olhar em silêncio se você não quiser que o barulho espante, diante de seus olhos, a beleza das coisas frágeis.", autor: "François Place", livro: "O velho louco por desenho"},
+  { frase: "Aprenda a olhar em silêncio se você não quiser que o barulho espante, diante de seus olhos, a beleza das coisas frágeis.", autor: "François Place", livro: "O velho louco por desenho" },
 
-  {frase: `<strong>Mafalda: </strong>Como foi na escola, Filipe? Já te ensinaram a escrever?<br>
+  {
+    frase: `<strong>Mafalda: </strong>Como foi na escola, Filipe? Já te ensinaram a escrever?<br>
   <strong>Filipe:</strong> E você queria que me ensinassem a escrever numa aula só?<br>
   <strong>Mafalda:</strong> Mas... você ficou lá o dia inteiro!<br>
   <strong>Filipe:</strong> É, mas antes a gente tem que fazer traços, letras, sílabas e um monte de outras coisas e... leva meses pra ensinarem a gente a escrever!<br>
-  <strong>Mafalda:</strong> <strong>MESES?! MALDITOS BUROCRATAS!</strong>`, autor: "Quino", livro: "Toda Mafalda"},
+  <strong>Mafalda:</strong> <strong>MESES?! MALDITOS BUROCRATAS!</strong>`, autor: "Quino", livro: "Toda Mafalda"
+  },
 
-  {frase: "A beleza será CONVULSIVA, ou não será", autor: "André Breton", livro: "Nadja"},
+  { frase: "A beleza será CONVULSIVA, ou não será", autor: "André Breton", livro: "Nadja" },
 
-  {frase: "Temos que resolver o seu problema, ele havia me dito aquela manhã, enquanto caminhávamos até o ponto. Perguntei qual, porque eu pensava que tinha muitos problemas, e não apenas um. Sua timidez, me respondeu, as mulheres não gostam dos tímidos. E claro que eu era tímido na época; falo de uma timidez genuína, verdadeira, não como hoje em dia que todo mundo é tímido, chega ser engraçado. Se alguém não te cumprimenta, dizem que é por timidez, e se matou a mulher, foi porque era tímido, se enganou um povo inteiro, se se candidatou a deputado, se comeu o resto de nutella do pote sem perguntar a ninguém: tímido. Falo de outra coisa: gagueira, insegurança, introspecção.", autor: "Alejandro Zambra", livro: "Meus documentos"},
+  { frase: "Temos que resolver o seu problema, ele havia me dito aquela manhã, enquanto caminhávamos até o ponto. Perguntei qual, porque eu pensava que tinha muitos problemas, e não apenas um. Sua timidez, me respondeu, as mulheres não gostam dos tímidos. E claro que eu era tímido na época; falo de uma timidez genuína, verdadeira, não como hoje em dia que todo mundo é tímido, chega ser engraçado. Se alguém não te cumprimenta, dizem que é por timidez, e se matou a mulher, foi porque era tímido, se enganou um povo inteiro, se se candidatou a deputado, se comeu o resto de nutella do pote sem perguntar a ninguém: tímido. Falo de outra coisa: gagueira, insegurança, introspecção.", autor: "Alejandro Zambra", livro: "Meus documentos" },
 
-  {frase: `a poesia mora <br> nos dias habita as frestas <br> das calçadas adormece no canto <br> entre cama e parede <br> e insiste <br> escorregando pelas sombras <br> dos sapatos apressados pelo sorriso preso <br> no canto do rosto <br> nos teus <br> e meus <br> olhos d'água`, autor: "Aline Miranda", livro: "..."},
+  { frase: `a poesia mora <br> nos dias habita as frestas <br> das calçadas adormece no canto <br> entre cama e parede <br> e insiste <br> escorregando pelas sombras <br> dos sapatos apressados pelo sorriso preso <br> no canto do rosto <br> nos teus <br> e meus <br> olhos d'água`, autor: "Aline Miranda", livro: "..." },
 
-  {frase: "Olha maninho, o essencial é fazermo-nos hábeis, e não úteis.", autor: "Mario Quintana", livro: "Nariz de vidro"},
+  { frase: "Olha maninho, o essencial é fazermo-nos hábeis, e não úteis.", autor: "Mario Quintana", livro: "Nariz de vidro" },
 
-  {frase: "...E como se reconhece, no fundo, a vida que vingou? Um homem que vingou faz bem a nossos sentidos: (...) Inventa meios de cura para injúrias, utiliza acasos ruins em seu proveito; o que não o mata o fortalece. De tudo o que vê, ouve e vive forma instintivamente sua soma. Ele é um princípio relativo, muito deixa de lado. Está sempre em sua companhia, lide com homens, livros ou paisagens: (...) Reage lentamente a toda sorte de estímulo, com aquela lentidão que uma larga previdência e um orgulho conquistado nele cultivaram - interroga o estímulo que se aproxima, está longe de ir ao seu encontro. Descrê de \"infortúnio\" como de culpa: acerta contas consigo, com os outros, sabe esquecer - é forte o bastante para que tudo tenha de resultar no melhor para ele.", autor: "Friedrich Nietzsche", livro:"Ecce homo"},
+  { frase: "...E como se reconhece, no fundo, a vida que vingou? Um homem que vingou faz bem a nossos sentidos: (...) Inventa meios de cura para injúrias, utiliza acasos ruins em seu proveito; o que não o mata o fortalece. De tudo o que vê, ouve e vive forma instintivamente sua soma. Ele é um princípio relativo, muito deixa de lado. Está sempre em sua companhia, lide com homens, livros ou paisagens: (...) Reage lentamente a toda sorte de estímulo, com aquela lentidão que uma larga previdência e um orgulho conquistado nele cultivaram - interroga o estímulo que se aproxima, está longe de ir ao seu encontro. Descrê de \"infortúnio\" como de culpa: acerta contas consigo, com os outros, sabe esquecer - é forte o bastante para que tudo tenha de resultar no melhor para ele.", autor: "Friedrich Nietzsche", livro: "Ecce homo" },
 
-  {frase:"Seu coração batia forte. Ah, como batia.", autor:"Karl Ove Knausgard", livro:"Minha luta 1: A morte do pai"},
+  { frase: "Seu coração batia forte. Ah, como batia.", autor: "Karl Ove Knausgard", livro: "Minha luta 1: A morte do pai" },
 
-  {frase:"Tudo que não é funcional é meramente decorativo.", autor:"David Mazzucchelli", livro:"Asterios Polyp"},
+  { frase: "Tudo que não é funcional é meramente decorativo.", autor: "David Mazzucchelli", livro: "Asterios Polyp" },
 
-  {frase:"Mas lembre-se para sempre de que os monstros não morrem. O que morre é o medo que eles incutem em você. Assim o é com os deuses. Quando os mortais não tiverem mais medo, os deuses desaparecerão.", autor:"Cesare Pavese", livro:"Diálogos com Leucó"},
+  { frase: "Mas lembre-se para sempre de que os monstros não morrem. O que morre é o medo que eles incutem em você. Assim o é com os deuses. Quando os mortais não tiverem mais medo, os deuses desaparecerão.", autor: "Cesare Pavese", livro: "Diálogos com Leucó" },
 
-  {frase:"Observe o rapaz que toma banho no Asopo. Manhã de verão. O rapaz sai da água, torna a entrar, mergulha e volta a mergulhar. Passa mal e se afoga. O que têm os deuses a ver com isso? Deverá atribuir o seu fim aos deuses ou então ao prazer desfrutado? Nem uma coisa nem outra. Aconteceu algo - que não é bom nem mau, alguma coisa que não tem nome -, depois os deuses lhe darão um nome.", autor:"Cesare Pavese", livro:"Diálogos com Leucó"},
+  { frase: "Observe o rapaz que toma banho no Asopo. Manhã de verão. O rapaz sai da água, torna a entrar, mergulha e volta a mergulhar. Passa mal e se afoga. O que têm os deuses a ver com isso? Deverá atribuir o seu fim aos deuses ou então ao prazer desfrutado? Nem uma coisa nem outra. Aconteceu algo - que não é bom nem mau, alguma coisa que não tem nome -, depois os deuses lhe darão um nome.", autor: "Cesare Pavese", livro: "Diálogos com Leucó" },
 
-  {frase:"Jamais para mim a bandeira abaixada, jamais a última tentativa.", autor:"Sir Ernest Shackleton", livro:"Endurance: a lendária expedição de Shackleton à Antártida"},
+  { frase: "Jamais para mim a bandeira abaixada, jamais a última tentativa.", autor: "Sir Ernest Shackleton", livro: "Endurance: a lendária expedição de Shackleton à Antártida" },
 
-  {frase:"Meu coração ventava forçando o peito.", autor:"Maura Lopes Cançado", livro:"O sofredor do ver"},
+  { frase: "Meu coração ventava forçando o peito.", autor: "Maura Lopes Cançado", livro: "O sofredor do ver" },
 
-  {frase:"Tu, que me lês, estás seguro de entender minha linguagem?", autor:"Jorge Luis Borges", livro:"Ficções"},
+  { frase: "Tu, que me lês, estás seguro de entender minha linguagem?", autor: "Jorge Luis Borges", livro: "Ficções" },
 
-  {frase:"Ensinamentos obtidos sem sofrimento são desprovidos de valor. Pois as pessoas jamais podem adquirir algo sem sacrifício.", autor:"Hiromu Arakawa", livro:"Fullmetal Alchemist vol. 1"},
+  { frase: "Ensinamentos obtidos sem sofrimento são desprovidos de valor. Pois as pessoas jamais podem adquirir algo sem sacrifício.", autor: "Hiromu Arakawa", livro: "Fullmetal Alchemist vol. 1" },
 
-  /*{frase: `Aceitar o castigo imerecido,<br />
-          Não por fraqueza, mas por altivez.<br />
-          No tormento mais fundo o teu gemido<br />
-          Trocar num grito de ódio a quem o fez.<br />
-          As delícias da carne e pensamento<br />
-          Com que o instinto da espécie nos engana<br />
-          Sobpor ao generoso sentimento<br />
-          De uma afeição mais simplesmente humana.<br />
-          Não tremer de esperança nem de espanto.<br />
-          Nada pedir nem desejar senão<br />
-          A coragem de ser um novo santo<br />
-          Sem fé num mundo além do mundo. E então<br />
-          &nbsp;&nbsp;Morrer sem uma lágrima, que a vida<br />
-          &nbsp;&nbsp;Não vale a pena e a dor de ser vivida.`, autor: "Manuel Bandeira", livro: "Soneto Inglês nº 2 | Estrela da vida inteira"}*/
+  { frase: `Aceitar o castigo imerecido,<br />
+            Não por fraqueza, mas por altivez.`,
+    autor: "Manuel Bandeira", livro: "Soneto Inglês nº 2 | Estrela da vida inteira"}
 ];
+
+console.log(selecao.length);
 
 function gerarSelecao() {
   const randomQuote = selecao[Math.floor(Math.random() * selecao.length)];
   document.getElementById('texto').innerHTML = randomQuote.frase;
-  document.getElementById('livro').innerHTML = `<strong><cite>${randomQuote.livro}</cite></strong>`;
+  document.getElementById('livro').innerHTML = `< strong > <cite>${randomQuote.livro}</cite></strong> `;
   document.getElementById('autor').textContent = randomQuote.autor;
 }
 
--- scripts/frases-e-trechos-2.js
+++ scripts/frases-e-trechos-2.js
@@ -1,36 +1,40 @@
 const selecao = [
-  {frase: "O menino baixou a cabeça, desolado. Sua memória o levou de volta à aldeia em torno do lago, onde tinha sido tão feliz, onde tivera a breve companhia de khuna. Lembrou o momento em que salvou a vida da sua inseparável Wintata, tingindo a face do filhote logo após o nascimento com corante de tecido para evitar que integrasse o rebanho de lhamas brancas, reservadas como oferenda aos deuses. Fez isso porque nunca assimilara muito bem o princípio dos rituais com sacrifícios.", autor: "CárcamO", livro: "Thapa Kunturi"},
+  { frase: "O menino baixou a cabeça, desolado. Sua memória o levou de volta à aldeia em torno do lago, onde tinha sido tão feliz, onde tivera a breve companhia de khuna. Lembrou o momento em que salvou a vida da sua inseparável Wintata, tingindo a face do filhote logo após o nascimento com corante de tecido para evitar que integrasse o rebanho de lhamas brancas, reservadas como oferenda aos deuses. Fez isso porque nunca assimilara muito bem o princípio dos rituais com sacrifícios.", autor: "CárcamO", livro: "Thapa Kunturi" },
 
-  {frase: "Nós, os pintores, os poetas, os escritores, os gravuristas, os atores, formávamos um mundo à parte. Éramos os representantes do 'mundo das nuvens', de tudo o que é vivo e se move.", autor: "François Place", livro: "O velho louco por desenho"},
+  { frase: "Nós, os pintores, os poetas, os escritores, os gravuristas, os atores, formávamos um mundo à parte. Éramos os representantes do 'mundo das nuvens', de tudo o que é vivo e se move.", autor: "François Place", livro: "O velho louco por desenho" },
 
-  {frase: "O que eu penso da Mafalda não importa. Importante mesmo é o que a Mafalda pensa de mim", autor: "Julio Cortázar", livro: "Toda Mafalda"},
+  { frase: "O que eu penso da Mafalda não importa. Importante mesmo é o que a Mafalda pensa de mim", autor: "Julio Cortázar", livro: "Toda Mafalda" },
 
-  {frase: "Viver (na minha compreensão) é existir dentro de um conceito de tempo. Mas relembrar é cancelar a própria noção de tempo. Toda lembrança, não importa o quão remota, acontece 'agora', no momento em que é convocada à mente. Quanto mais algo é relembrado, mais vezes o cérebro tem chance de aprimorar a experiência original, porque toda lembrança é uma recriação, não uma reprodução.", autor: "David Mazzucchelli", livro: "Asterios Polyp"},
+  { frase: "Viver (na minha compreensão) é existir dentro de um conceito de tempo. Mas relembrar é cancelar a própria noção de tempo. Toda lembrança, não importa o quão remota, acontece 'agora', no momento em que é convocada à mente. Quanto mais algo é relembrado, mais vezes o cérebro tem chance de aprimorar a experiência original, porque toda lembrança é uma recriação, não uma reprodução.", autor: "David Mazzucchelli", livro: "Asterios Polyp" },
 
-  {frase: "Observo os carros, conto os carros. Me parece triste pensar que nos assentos traseiros vão meninos dormindo, e que cada um desses meninos recordará, alguma vez, o antigo carro em que anos atrás viajava com seus pais.", autor: "Alejandro Zambra", livro: "Formas de voltar para casa"},
+  { frase: "Observo os carros, conto os carros. Me parece triste pensar que nos assentos traseiros vão meninos dormindo, e que cada um desses meninos recordará, alguma vez, o antigo carro em que anos atrás viajava com seus pais.", autor: "Alejandro Zambra", livro: "Formas de voltar para casa" },
 
-  {frase: "Gostou do fato de no estádio as pessoas gritarem e reclamarem, mas ficou surpreso quando notou a raiva dirigida ao árbitro, de modo que resolveu defendê-lo, e ainda que no começo as pessoas tenham levado a mal, também era engraçado ver o Camilo, a cada vez que o árbitro marcava uma falta ou dava um cartão, ficar de pé e dizer em voz alta: muito bem, senhor juiz, ótima decisão.", autor: "Alejandro Zambra", livro: "Meus documentos"},
+  { frase: "Gostou do fato de no estádio as pessoas gritarem e reclamarem, mas ficou surpreso quando notou a raiva dirigida ao árbitro, de modo que resolveu defendê-lo, e ainda que no começo as pessoas tenham levado a mal, também era engraçado ver o Camilo, a cada vez que o árbitro marcava uma falta ou dava um cartão, ficar de pé e dizer em voz alta: muito bem, senhor juiz, ótima decisão.", autor: "Alejandro Zambra", livro: "Meus documentos" },
 
-  {frase: `Quase falara com ela mas aqueles olhos haviam alterado o mundo para sempre no espaço de uma batida do coração.`, autor: "Cormac McCarthy", livro: "Todos os belos cavalos"},
+  { frase: `Quase falara com ela mas aqueles olhos haviam alterado o mundo para sempre no espaço de uma batida do coração.`, autor: "Cormac McCarthy", livro: "Todos os belos cavalos" },
 
-  {frase: "Coragem, não é cedendo ao ócio nem refestelando-se sobre plumas que se conquistam os prêmios ao valor. Aquele que à inatividade se entregar, de si deixará sobre a terra memória igual ao traço que o fumo risca no ar e a espuma traça na onda. Supera a fadiga, vence o torpor, recobra o ânimo, que das vitórias sobre os perigos, a primeira é a da vontade sobre o corpo.", autor: "Dante Alighieri", livro: "A divina comédia"},
+  { frase: "Coragem, não é cedendo ao ócio nem refestelando-se sobre plumas que se conquistam os prêmios ao valor. Aquele que à inatividade se entregar, de si deixará sobre a terra memória igual ao traço que o fumo risca no ar e a espuma traça na onda. Supera a fadiga, vence o torpor, recobra o ânimo, que das vitórias sobre os perigos, a primeira é a da vontade sobre o corpo.", autor: "Dante Alighieri", livro: "A divina comédia" },
 
-  {frase: "Falando simples, odeio a todos os Deuses que bem tratados afligem-me sem justiça.", autor:"Ésquilo", livro:"Prometeu acorrentado"},
+  { frase: "Falando simples, odeio a todos os Deuses que bem tratados afligem-me sem justiça.", autor: "Ésquilo", livro: "Prometeu acorrentado" },
 
-  {frase:`(...) que enquanto vagueio pelas cinzas do mundo <br /> com inúteis pés fixados, enquanto sofro <br /> e sofrendo me solto e me recomponho <br /> e torno a viver e ando (...)`, autor:"Carlos Drummond de Andrade", livro:"Antologia poética"},
+  { frase: `(...) que enquanto vagueio pelas cinzas do mundo <br /> com inúteis pés fixados, enquanto sofro <br /> e sofrendo me solto e me recomponho <br /> e torno a viver e ando (...)`, autor: "Carlos Drummond de Andrade", livro: "Antologia poética" },
 
-  {frase:"A que serve viver os dias se eles não são recordados?", autor:"Cesare Pavese", livro:"Diálogos com Leucó"},
+  { frase: "A que serve viver os dias se eles não são recordados?", autor: "Cesare Pavese", livro: "Diálogos com Leucó" },
 
-  {frase:"Peço para viver, não para ser feliz.", autor:"Cesare Pavese", livro:"Diálogos com Leucó"},
+  { frase: "Peço para viver, não para ser feliz.", autor: "Cesare Pavese", livro: "Diálogos com Leucó" },
 
-  {frase:"Ela deu-lhe a mão e mencionou numa pergunta o tédio e a solidão.", autor:"Juan Carlos Onetti", livro:"47 contos de Juan Carlos Onetti"},
+  { frase: "Ela deu-lhe a mão e mencionou numa pergunta o tédio e a solidão.", autor: "Juan Carlos Onetti", livro: "47 contos de Juan Carlos Onetti" },
 
-  {frase:"Rendo-me. Que aconteça o que tiver que acontecer. Não vou mais me agarrar a nada.", autor:"Natsume Soseki", livro:"Eu sou um gato"},
+  { frase: "Rendo-me. Que aconteça o que tiver que acontecer. Não vou mais me agarrar a nada.", autor: "Natsume Soseki", livro: "Eu sou um gato" },
 
-  {frase:"Só é verdade o que acontece a cada trezentas noites.", autor:"Jorge Luis Borges", livro:"Ficções"},
+  { frase: "Só é verdade o que acontece a cada trezentas noites.", autor: "Jorge Luis Borges", livro: "Ficções" },
 
-  {frase:"(...) tinha vivido como quem sonha: olhava sem ver, ouvia sem ouvir, esquecia-se de tudo, de quase tudo.", autor:"Jorge Luis Borges", livro:"Ficções"}
+  { frase: "(...) tinha vivido como quem sonha: olhava sem ver, ouvia sem ouvir, esquecia-se de tudo, de quase tudo.", autor: "Jorge Luis Borges", livro: "Ficções" },
+
+  { frase: "O impossível é uma fronteira que sempre recua um pouco.", autor: "Victor Hugo", livro: "Os trabalhadores do mar" },
 ]
+
+console.log(selecao.length);
 
 function gerarSelecao() {
   const randomQuote = selecao[Math.floor(Math.random() * selecao.length)];
@@ -39,6 +43,6 @@ function gerarSelecao() {
   document.getElementById('autor').textContent = randomQuote.autor;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   gerarSelecao(); // Chamar a função ao carregar a página
 });
